6a1053ca6853517436e579ff2911493cd18add58
Matumadrid <140453169+Matumadrid@users.noreply.github.com>
2024-06-10 01:19:49 -0300

    Add files via upload

---
+++ models/carreras.js
@@ -0,0 +1,36 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Carreras extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Carreras.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    grado: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    universidad: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize, // Sequelize instance should be passed here
+    modelName: 'Carreras', // Model name should be defined here
+    tableName: 'carreras', // Optionally, specify the table name if different
+    timestamps: false // Optionally, specify if timestamps are not used
+  });
+  return Carreras;
+};
