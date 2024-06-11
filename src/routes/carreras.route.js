fb45cee05a69b13e80393acea31350a272d383f7
Matumadrid <140453169+Matumadrid@users.noreply.github.com>
2024-06-10 23:49:18 -0300

    Add files via upload

--- src/routes/carreras.route.js
+++ src/routes/carreras.route.js
@@ -3,11 +3,14 @@ const carrerasController = require('../controllers/carreras.controller')
 const carrerasMiddleware = require('../middlewares/carreras.middleware')
 const schemaValidator = require('../middlewares/schemaValidator')
 const carrerasSchema = require('../schemas/carreras.schema')
+const materiasSchema = require('../schemas/materias.schema')
 const route = Router()
 
 route.get('/carreras', carrerasController.getAllcarreras)
 route.get('/carreras/:id', carrerasMiddleware.existeCarreraById, carrerasController.getCarreraById)
 route.post('/carreras', schemaValidator(carrerasSchema), carrerasController.createCarrera)
+route.post('/carreras/:id/materia', schemaValidator(materiasSchema), carrerasController.createMateriaByCarreraId)
+route.get('/carreras/:id/materias', carrerasMiddleware.existeCarreraById, carrerasController.getAllMateriasByCarreraId)
 route.delete('/carreras/:id', carrerasMiddleware.existeCarreraById, carrerasController.deleteCarrera)
 
 module.exports = route  
